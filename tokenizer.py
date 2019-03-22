62344022cff560dc9ff96bb2c46bdcd60bd1f894
agw2105 <48716562+agw2105@users.noreply.github.com>
2019-03-22 17:40:42 -0400

    Add files via upload
    tokenize and stemming module

---
+++ tokenizer.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 10:50:52 2019
+
+"""
+import nltk
+import sklearn
+import re
+from nltk.stem.snowball import SnowballStemmer
+
+stemmer = SnowballStemmer("english")
+stopwords = nltk.corpus.stopwords.words('english')
+
+def tokenize_and_stem(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    stems = [stemmer.stem(t) for t in filtered_tokens]
+    return stems
+
+def tokenize_only(text):
+    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    return filtered_tokens
